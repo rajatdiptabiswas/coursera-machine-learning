c318d2738f1c1cf0f1820b174c63745a447a8ebd
Rajat <rdbiswas97@gmail.com>
2019-02-17 11:19:12 +0530

    Week 8 assignments

---
+++ machine-learning-ex7/ex7/pca.m
@@ -0,0 +1,35 @@
+function [U, S] = pca(X)
+%PCA Run principal component analysis on the dataset X
+%   [U, S, X] = pca(X) computes eigenvectors of the covariance matrix of X
+%   Returns the eigenvectors U, the eigenvalues (on diagonal) in S
+%
+
+% Useful values
+[m, n] = size(X);
+
+% You need to return the following variables correctly.
+U = zeros(n);
+S = zeros(n);
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: You should first compute the covariance matrix. Then, you
+%               should use the "svd" function to compute the eigenvectors
+%               and eigenvalues of the covariance matrix. 
+%
+% Note: When computing the covariance matrix, remember to divide by m (the
+%       number of examples).
+%
+
+% m examples
+% n dimensional data
+
+% covariance matrix
+sigma_matrix = (1/m) * X' * X;
+
+% compute principal component analysis using singular value decomposition
+[U, S, V] = svd(sigma_matrix);
+
+
+% =========================================================================
+
+end
